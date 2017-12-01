b797963053ee0bce55617ff1330d6696f46ae0e7
WANGYUNYI <wangyunyi@casfive.com>
2017-12-01 18:33:22 +0800

    a

--- ManualAlgorithm/tree.py
+++ ManualAlgorithm/tree.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 
-class DecisionTree(object):
+class BaseTree(object):
     def __init__(self):
         pass
-
-    def __cal_entropy(self,x,y,solver='entropy'):
+    def __cal_entropy(self,x,y):
         #entropy
         #entropyrate
         #gini
@@ -14,7 +13,34 @@ class DecisionTree(object):
         y = np.array(y)
         label_unique = np.unique(y)
 
-        assert np.sum(np.unique(y)) == 1,"检查输入的标签列是否为0和1"
+        assert np.sum(label_unique) == 1,"检查输入的标签列是否为0和1"
+        m = len(x)
+        m1 = np.sum(y)
+        m0 = m - m1
+        hd = (-m1/m) * np.log2(m1/m) + (-m0/m) * np.log2(m0/m)
+        return hd
+
+
+
+
+
+
+class DecisionTree(object):
+    def __init__(self,max_depth = 3,solver='entropy'):
+
+        self.max_depth = max_depth
+        self.solver = 'entropy'
+
+    def __cal_entropy(self,x,y):
+        #entropy
+        #entropyrate
+        #gini
+
+        x = np.array(x)
+        y = np.array(y)
+        label_unique = np.unique(y)
+
+        assert np.sum(label_unique) == 1,"检查输入的标签列是否为0和1"
         m = len(x)
         m1 = np.sum(y)
         m0 = m - m1
@@ -22,7 +48,8 @@ class DecisionTree(object):
 
         return hd
 
-
+    def fit(self,x,y):
+        __data_hd = self.__cal_entropy(x,y)
 
 
 
